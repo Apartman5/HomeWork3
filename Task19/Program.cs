d4f9327ab71a52a744c56c74078113e42e43a00c
Igor Kazakov <kiw@lefterova.ru>
2023-03-13 14:26:57 +0500

    first commit

---
+++ Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №19
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// Примеры:
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+int CheckNum() // проверяет, является ли введенное значение числом и его разрядность
+{
+    Console.WriteLine("Введите разрядность числа: ");
+    int digit = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число требуемой разрядности: ");
+    string number = Console.ReadLine();
+    string[] numArr = new string [number]
+    for (int i = 0; i < digit; i++)
+    {
+        
+    }
+
+    if (number )
+}
