c3a101890456e387d605c84402b260d1f6a69545
Igor Kazakov <kiw@lefterova.ru>
2023-03-14 15:44:38 +0500

    second commit

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,3 +6,23 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Введите 5-значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string newNumber = Convert.ToString(number);
+if(newNumber.Length != 5)
+    {
+        Console.WriteLine("Вы ввели неправильное число!");
+    }
+    else
+    {
+        if (newNumber[0] == newNumber[4] && newNumber[1] == newNumber[3])
+                Console.WriteLine("Это полиндром");
+            else Console.WriteLine("это не полиндром");
+    }
+        
+    
+
+
+
+
---
+++ Task21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// Примеры:
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double[] pointA = new double [3];
+double[] pointB = new double [3];
+double distance = 0;
+
+Console.WriteLine("введите координаты первой точки - X, Y, Z");
+for (int i = 0; i < pointA.Length; i ++)
+    pointA[i] = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите координаты второй точки - X, Y, Z");
+for (int i = 0; i < pointB.Length; i ++)
+    pointB[i] = Convert.ToDouble(Console.ReadLine());
+
+distance = Math.Sqrt(Math.Pow(pointA[0] - pointB[0], 2) + Math.Pow(pointA[1] - pointB[1], 2) + Math.Pow(pointA[2] - pointB[2], 2));
+Console.WriteLine($"Растояние между точками равно {Math.Round(distance, 2)}");
